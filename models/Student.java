eb92b34ee534220ef17709f0154453f5e3233040
schelihaeva <146697334+schelihaeva@users.noreply.github.com>
2024-10-07 13:07:41 +0300

    Update Student.java

--- models/Student.java
+++ models/Student.java
@@ -7,7 +7,7 @@ import java.util.Random;
 @Table(name = "students")
 public class Student {
 
-    private static final String[] names = new String[] { "Анатолий", "Глеб", "Клим", "Мартин", "Лазарь", "Владлен", "Клим", "Панкратий", "Рубен", "Герман" };
+    private static final String[] names = new String[] { "Савелий", "Арте", "Алексей", "Никита", "Илья", "Дмитрий", "Саша", "Антон", "Руслан", "Гоша" };
     private static final Random random = new Random();
 
     @Id
